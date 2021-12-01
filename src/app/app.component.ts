257c200bf4efca2e38f9d21a8a9b258106930da0
Lopez Facundo <facueliaslopez@hotmail.com>
2021-12-01 01:38:19 -0300

    video 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'aplicacion ',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'mi aplicación';
+  nombre = "Facu"
 }
